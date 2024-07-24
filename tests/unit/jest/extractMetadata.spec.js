16ace15e1277fa58c8718b3bbce21d274bd4d242
Prajwol Amatya <prajwolamatya11@gmail.com>
2024-07-24 14:12:52 +0545

    define functions as method property and add unit tests

--- tests/unit/jest/extractMetadata.spec.js
+++ tests/unit/jest/extractMetadata.spec.js
@@ -1,58 +1,59 @@
 import fs from 'fs'
-import Plugin from '../../../plugin/awesoMD/plugin'
+import plugin from '../../../plugin/awesoMD/plugin'
 
-const plugin = Plugin()
+const mdPlugin = plugin()
 
 afterEach(() => {
     jest.restoreAllMocks()
 })
 
-describe("separateInlineMetadataAndMarkdown", () => {
+describe('separateInlineMetadataAndMarkdown', () => {
+    let slideOptions = {
+        metadata: {
+            description: 'some description',
+            footer: 'footer content',
+            slide: 'title-content',
+            presenter: 'presenter name',
+        },
+    }
+
     it.each([
         [
             [
-                '```yaml\n' +
-                'slide: cover\n' +
-                'toc: false\n' +
-                '```\n' +
-                '# Cover Slide',
-                '```yaml\n' +
-                'slide: section\n' +
-                '```\n' +
-                '# Section Slide',
-                '# Title Content Slide\n' +
-                'some content'
+                '```yaml\n' + 'slide: cover\n' + 'toc: false\n' + '```\n' + '# Cover Slide',
+                '```yaml\n' + 'slide: section\n' + '```\n' + '# Section Slide',
+                '# Title Content Slide\n' + 'some content',
             ],
             [
                 [
                     {
-                        "description": "some description",
-                        "footer": "footer content",
-                        "slide": "cover",
-                        "presenter": "presenter name",
-                        "toc": false
+                        description: 'some description',
+                        footer: 'footer content',
+                        slide: 'cover',
+                        presenter: 'presenter name',
+                        toc: false,
                     },
-                    '# Cover Slide'
+                    '# Cover Slide',
                 ],
                 [
                     {
-                        "description": "some description",
-                        "footer": "footer content",
-                        "slide": "section",
-                        "presenter": "presenter name"
+                        description: 'some description',
+                        footer: 'footer content',
+                        slide: 'section',
+                        presenter: 'presenter name',
                     },
-                    '# Section Slide'
+                    '# Section Slide',
                 ],
                 [
                     {
-                        "description": "some description",
-                        "footer": "footer content",
-                        "slide": "title-content",
-                        "presenter": "presenter name"
+                        description: 'some description',
+                        footer: 'footer content',
+                        slide: 'title-content',
+                        presenter: 'presenter name',
                     },
-                    '# Title Content Slide\nsome content'
-                ]
-            ]
+                    '# Title Content Slide\nsome content',
+                ],
+            ],
         ],
         [
             [
@@ -60,82 +61,141 @@ describe("separateInlineMetadataAndMarkdown", () => {
                 '# Section Slide ::slide:section',
                 '# Title Content Slide\nsome content',
                 '# Title with colon slide:notavalue ::slide:cover ::toc:false',
-                '# Cover Slide ::slide:cover ::toc:false ::toc:true'
+                '# Cover Slide ::slide:cover ::toc:false ::toc:true',
             ],
             [
                 [
                     {
-                        "description": "some description",
-                        "footer": "footer content",
-                        "slide": "cover",
-                        "presenter": "presenter name",
-                        "toc": "false"
+                        description: 'some description',
+                        footer: 'footer content',
+                        slide: 'cover',
+                        presenter: 'presenter name',
+                        toc: 'false',
                     },
-                    '# Cover Slide'
+                    '# Cover Slide',
                 ],
                 [
                     {
-                        "description": "some description",
-                        "footer": "footer content",
-                        "slide": "section",
-                        "presenter": "presenter name"
+                        description: 'some description',
+                        footer: 'footer content',
+                        slide: 'section',
+                        presenter: 'presenter name',
                     },
-                    '# Section Slide'
+                    '# Section Slide',
                 ],
                 [
                     {
-                        "description": "some description",
-                        "footer": "footer content",
-                        "slide": "title-content",
-                        "presenter": "presenter name"
+                        description: 'some description',
+                        footer: 'footer content',
+                        slide: 'title-content',
+                        presenter: 'presenter name',
                     },
-                    '# Title Content Slide\nsome content'
+                    '# Title Content Slide\nsome content',
                 ],
                 [
                     {
-                        "description": "some description",
-                        "footer": "footer content",
-                        "slide": "cover",
-                        "presenter": "presenter name",
-                        "toc": "false"
+                        description: 'some description',
+                        footer: 'footer content',
+                        slide: 'cover',
+                        presenter: 'presenter name',
+                        toc: 'false',
                     },
-                    '# Title with colon slide:notavalue'
+                    '# Title with colon slide:notavalue',
                 ],
                 [
                     {
-                        "description": "some description",
-                        "footer": "footer content",
-                        "slide": "cover",
-                        "presenter": "presenter name",
-                        "toc": "true"
+                        description: 'some description',
+                        footer: 'footer content',
+                        slide: 'cover',
+                        presenter: 'presenter name',
+                        toc: 'true',
                     },
-                    '# Cover Slide'
-                ]
+                    '# Cover Slide',
+                ],
+            ],
+        ],
+    ])(
+        'should separate and return expected metadata and markdown content',
+        (slideContent, expectedSeparatedMetadataAndMarkdown) => {
+            const spySeparateInlineMetadataAndMarkdown = jest.spyOn(mdPlugin, 'separateInlineMetadataAndMarkdown')
+            slideContent.forEach((slide, index) => {
+                const [content, options] = mdPlugin.separateInlineMetadataAndMarkdown(slide, { ...slideOptions })
+                expect(options.metadata).toEqual(expectedSeparatedMetadataAndMarkdown[index][0])
+                expect(content.trim()).toEqual(expectedSeparatedMetadataAndMarkdown[index][1])
+            })
+
+            expect(spySeparateInlineMetadataAndMarkdown).toHaveBeenCalledTimes(slideContent.length)
+        }
+    )
+
+    it.each([true, false])(
+        'should execute extractInlineMetadata when separator by heading is true',
+        (separateByHeading) => {
+            slideOptions = {
+                ...slideOptions,
+                separateByHeading: separateByHeading,
+            }
+
+            const metadataSlide = [
+                '# Cover Slide ::slide:cover ::toc:false',
+                '# Section Slide ::slide:section',
+                '# Title Content Slide\nsome content',
             ]
-        ]
-    ])('should separate and return expected metadata and markdown content', (slideContent, expectedSeparatedMetadataAndMarkdown) => {
-        const slideOptions = {
-            "metadata": {
-                "description": "some description",
-                "footer": "footer content",
-                "slide": "title-content",
-                "presenter": "presenter name"
+
+            const expectedMetadataAndMarkdown = [
+                [
+                    {
+                        description: 'some description',
+                        footer: 'footer content',
+                        slide: 'cover',
+                        presenter: 'presenter name',
+                        toc: 'false',
+                    },
+                    '# Cover Slide',
+                ],
+                [
+                    {
+                        description: 'some description',
+                        footer: 'footer content',
+                        slide: 'section',
+                        presenter: 'presenter name',
+                    },
+                    '# Section Slide',
+                ],
+                [
+                    {
+                        description: 'some description',
+                        footer: 'footer content',
+                        slide: 'title-content',
+                        presenter: 'presenter name',
+                    },
+                    '# Title Content Slide\nsome content',
+                ],
+            ]
+
+            const spySeparateInlineMetadataAndMarkdown = jest.spyOn(mdPlugin, 'separateInlineMetadataAndMarkdown')
+            const spyExtractInlineMetadata = jest.spyOn(mdPlugin, 'extractInlineMetadata')
+            const spyExtractYAMLMetadata = jest.spyOn(mdPlugin, 'extractYAMLMetadata')
+            metadataSlide.forEach((slide, index) => {
+                const [content, options] = mdPlugin.separateInlineMetadataAndMarkdown(slide, { ...slideOptions })
+                expect(options.metadata).toEqual(expectedMetadataAndMarkdown[index][0])
+                expect(content.trim()).toEqual(expectedMetadataAndMarkdown[index][1])
+            })
+
+            expect(spySeparateInlineMetadataAndMarkdown).toHaveBeenCalledTimes(metadataSlide.length)
+
+            if (separateByHeading) {
+                expect(spyExtractInlineMetadata).toHaveBeenCalledTimes(metadataSlide.length)
+            } else {
+                expect(spyExtractYAMLMetadata).not.toHaveBeenCalled()
             }
         }
-        const spySeparateInlineMetadataAndMarkdown = jest.spyOn(plugin, 'separateInlineMetadataAndMarkdown')
-        slideContent.forEach((slide, index) => {
-            const [content, options] = spySeparateInlineMetadataAndMarkdown(slide, { ...slideOptions })
-            expect(options.metadata).toEqual(expectedSeparatedMetadataAndMarkdown[index][0])
-            expect(content.trim()).toEqual(expectedSeparatedMetadataAndMarkdown[index][1])
-        })
-
-        expect(spySeparateInlineMetadataAndMarkdown).toHaveBeenCalledTimes(slideContent.length)
-    })
+    )
 })
 
 describe('addSlideSeparator', () => {
     const options = {
-        "slideSeparator": "---"
+        slideSeparator: '---',
     }
     const expectedMarkdownContent = `---
 description: some description
@@ -155,8 +215,7 @@ some content
 
     it('should add slide separator', () => {
         const markdownContent = fs.readFileSync('tests/unit/testFiles/noSlideSeparator.md', 'utf-8')
-        const spyAddSlideSeparator = jest.spyOn(plugin, 'addSlideSeparator')
-        const updatedMarkdownContent = spyAddSlideSeparator(markdownContent, options)
+        const updatedMarkdownContent = mdPlugin.addSlideSeparator(markdownContent, options)
         expect(updatedMarkdownContent).toEqual(expectedMarkdownContent)
     })
 })
